8438472566d9dfd0a6c3535202d8bb9611e60900
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-06-29 15:02:44 -0500

    refactor: update data source file names for better readability

--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ import json
 DATASET_DIR = os.path.join(os.path.dirname(__file__), "dataset")
 
 def load_data():
-    with open(os.path.join(DATASET_DIR, "movies_filtradas.json"), encoding="utf-8") as f:
+    with open(os.path.join(DATASET_DIR, "movies.json"), encoding="utf-8") as f:
         movies = json.load(f)
 
-    with open(os.path.join(DATASET_DIR, "persons_filtradas.json"), encoding="utf-8") as f:
+    with open(os.path.join(DATASET_DIR, "persons.json"), encoding="utf-8") as f:
         persons = json.load(f)
 
-    with open(os.path.join(DATASET_DIR, "principals_filtrados.json"), encoding="utf-8") as f:
+    with open(os.path.join(DATASET_DIR, "principals.json"), encoding="utf-8") as f:
         principals = json.load(f)
 
     return movies, persons, principals
